1ba0819ee8a572ee42609443a3d3414a3e51d984
Charles Leahan <charlesleahan@gmail.com>
2021-02-17 18:01:31 -0500

    Messages

--- app/mod_auth/controllers.py
+++ app/mod_auth/controllers.py
@@ -2,9 +2,9 @@ from flask import Flask, Blueprint, render_template, redirect, url_for,flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user,current_user 
 
-from app import db, login_manager
+from app import db, login_manager, User
 from app.mod_auth.forms import RegisterForm, LoginForm
-from app.mod_auth.models import User
+
 
 
 mod_auth = Blueprint('auth', __name__)
